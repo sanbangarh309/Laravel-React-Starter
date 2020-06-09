5f2f6de94bb9f8e35bff42d8f26bba191126499f
Sandeep Singh <ssingh@crebos.online>
2020-06-09 09:14:18 +0530

    upgraded to laravel 7 and react 16

---
+++ resources/js/modules/web/pages/home/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const displayName = "HomePageHeader"
+
+function Header() {
+  return <header className="bg-primary text-white">
+    <div className="container text-center">
+      <h1>Ap affiliate</h1>
+    </div>
+  </header>
+}
+Header.displayName = displayName
+
+export default Header
