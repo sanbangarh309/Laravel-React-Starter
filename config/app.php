99a06a6524f700c247cd4ea1ca6c22f4b958b197
Sandeep Bangarh <35298606+sanbangarh309@users.noreply.github.com>
2020-06-30 12:14:32 +0530

    update app name

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'AP Affiliate',
+    'name' => 'Laravel 7 && React 16',
 
     /*
     |--------------------------------------------------------------------------
